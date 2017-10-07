90f2705611b8d403ae2b5b133ac1e3c4556884e0
pepperpapa <zhongxin2506@outlook.com>
2017-10-07 17:00:32 +0800

    part7

--- polls/models.py
+++ polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
         now = timezone.now()
         return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
